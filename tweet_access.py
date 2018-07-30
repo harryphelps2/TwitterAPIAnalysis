9f3f70aa358eb0c2c78ad8e6d462cc0cec274fc5
harryphelps <harryphelps1@gmail.com>
2018-07-30 07:43:06 +0000

    Harvesting tweets

--- tweet_access.py
+++ tweet_access.py
@@ -13,18 +13,11 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-DUB_WOE_ID = 560743
-LON_WOE_ID = 44418
+count = 10
+query = 'Dublin'
 
-dub_trends = api.trends_place(DUB_WOE_ID)
-lon_trends = api.trends_place(LON_WOE_ID)
+#Get all status
+results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
 
-dub_trends_set = set([trend['name']
-                    for trend in dub_trends[0]['trends']])
-
-lon_trends_set = set([trend['name']
-                    for trend in lon_trends[0]['trends']])
-
-common_trends = set.intersection(dub_trends_set, lon_trends_set)
-
-print(common_trends)
+for result in results:
+    print(json.dumps(result._json, indent=2))
